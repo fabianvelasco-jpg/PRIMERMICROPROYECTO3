8d2bbc4b59a7d23e8feda902221b12dc5246c8ce
fabianvelasco-jpg <fabianvelasco@unicauca.edu.co>
2026-09-24 16:39:13 -0500

    aquí implementé un antirebote en otro process sugerido por la IA para no tener señales falsas del borón

--- codigo/cronometro_959.vhd
+++ codigo/cronometro_959.vhd
@@ -4,6 +4,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity cronometro_959 is
     port (
+		  clk_50Mhz   : in  std_logic;
         relojBase   : in  std_logic;
         boton_unico : in  std_logic;
         unidadesSec : out std_logic_vector(3 downto 0);
@@ -16,31 +17,52 @@ architecture logica of cronometro_959 is
     signal cuentaUniSec : unsigned(3 downto 0) := (others => '0');
     signal cuentaDecSec : unsigned(3 downto 0) := (others => '0');
     signal cuentaMin    : unsigned(3 downto 0) := (others => '0');
-    signal estadoActivo : std_logic := '1';
+    signal estadoActivo : std_logic := '0';
+	 signal orden_reset  : std_logic := '0';
     
-   
+    signal filtro_ruido : integer range 0 to 100000005 := 0;
     signal cuenta_boton : integer range 0 to 3 := 0;
 begin
-    process (relojBase,boton_unico)
+process (clk_50Mhz)
+    begin
+        if clk_50Mhz'event and clk_50Mhz = '1' then
+            if boton_unico = '0' then
+                
+                if filtro_ruido < 50000 then
+                    filtro_ruido <= filtro_ruido + 1; 
+                    
+                elsif filtro_ruido = 50000 then
+                    estadoActivo <= not estadoActivo; 
+                    filtro_ruido <= filtro_ruido + 1;
+                    
+                elsif filtro_ruido < 100000000 then
+                    filtro_ruido <= filtro_ruido + 1;
+                    
+                elsif filtro_ruido = 100000000 then
+                    orden_reset <= '1';              
+                    estadoActivo <= '0';
+                    filtro_ruido <= 100000001;     
+                end if;
+                
+            else
+                filtro_ruido <= 0;
+                orden_reset  <= '0';
+            end if;
+        end if;
+    end process;
+	 
+    process (relojBase,orden_reset)
     begin
         
       -- if general      
-		if boton_unico = '0' then -- empiza a evaluar si mi botón ya fue precionado o no
-            if cuenta_boton = 0 then
-                estadoActivo <= not estadoActivo;
-                cuenta_boton <= 1;
-            elsif cuenta_boton = 1 then
-                cuentaUniSec <= (others => '0');
-                cuentaDecSec <= (others => '0');
-                cuentaMin    <= (others => '0');
-                estadoActivo <= '0';
-                cuenta_boton <= 2;
-            end if;
+		if orden_reset = '1' then
+            cuentaUniSec <= (others => '0');
+            cuentaDecSec <= (others => '0');
+            cuentaMin    <= (others => '0');
 			-- contador 959, empieza evaluando si el reloj está en un flanco de subida
         elsif relojBase'event and relojBase = '1' then
             if estadoActivo = '1' then
                 if cuentaMin = 9 and cuentaDecSec = 5 and cuentaUniSec = 9 then
-                    estadoActivo <= '0'; 	-- evalua si el estado activo es cero (activo)
                 else
                     if cuentaUniSec = 9 then		-- mira si hemos llegado al limite de las unidades y 
 																-- suma una decena, y si hemos llegado al limite de las decenas entonces se agrega un
