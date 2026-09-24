9f1a0885d50fe806cd0f371631b885b686de9920
fabianvelasco-jpg <fabianvelasco@unicauca.edu.co>
2026-09-23 23:48:28 -0500

    corrección de retardo y comentarios de las partes del codigo

--- codigo/cronometro_959.vhd
+++ codigo/cronometro_959.vhd
@@ -5,7 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity cronometro_959 is
     port (
         relojBase   : in  std_logic;
-        boton_unico : in  std_logic; -- Único control (Active-Low)
+        boton_unico : in  std_logic;
         unidadesSec : out std_logic_vector(3 downto 0);
         decenasSec  : out std_logic_vector(3 downto 0);
         unidadesMin : out std_logic_vector(3 downto 0)
@@ -18,59 +18,50 @@ architecture logica of cronometro_959 is
     signal cuentaMin    : unsigned(3 downto 0) := (others => '0');
     signal estadoActivo : std_logic := '1';
     
-    -- Memoria de tiempo de pulsación (cuenta latidos de 1Hz)
+   
     signal cuenta_boton : integer range 0 to 3 := 0;
 begin
     process (relojBase,boton_unico)
     begin
         
-            
-           if boton_unico = '0' then
-
-                if cuenta_boton = 0 then
-                    -- Primer latido con el botón: alterna start/stop YA
-                    estadoActivo <= not estadoActivo;
-                    cuenta_boton <= 1;
-
-                elsif cuenta_boton = 1 then
-                    -- Segundo latido seguido: RESET
-                    cuentaUniSec <= (others => '0');
-                    cuentaDecSec <= (others => '0');
-                    cuentaMin    <= (others => '0');
-                    estadoActivo <= '0';
-                    cuenta_boton <= 2;
-                end if;
-
-                elsif
-						if relojBase'event and relojBase = '1' then
-                    -- Solo avanza si ya estaba activo desde el latido anterior
-                    if estadoActivo = '1' then
-                        if cuentaMin = 9 and cuentaDecSec = 5 and cuentaUniSec = 9 then
-                            estadoActivo <= '0'; -- Límite máximo
-                        else
-                            if cuentaUniSec = 9 then
-                                cuentaUniSec <= (others => '0');
-                                
-                                if cuentaDecSec = 5 then
-                                    cuentaDecSec <= (others => '0');
-                                    cuentaMin <= cuentaMin + 1;
-                                else
-                                    cuentaDecSec <= cuentaDecSec + 1;
-                                end if;
-                            else
-                                cuentaUniSec <= cuentaUniSec + 1;
-                           end if;
-                       end if;
-                  end if;
-                end if;
-              
-                -- Se borra la memoria del botón obligatoriamente al final
-                cuenta_boton <= 0;
-              end if;  
+      -- if general      
+		if boton_unico = '0' then -- empiza a evaluar si mi botón ya fue precionado o no
+            if cuenta_boton = 0 then
+                estadoActivo <= not estadoActivo;
+                cuenta_boton <= 1;
+            elsif cuenta_boton = 1 then
+                cuentaUniSec <= (others => '0');
+                cuentaDecSec <= (others => '0');
+                cuentaMin    <= (others => '0');
+                estadoActivo <= '0';
+                cuenta_boton <= 2;
             end if;
+			-- contador 959, empieza evaluando si el reloj está en un flanco de subida
+        elsif relojBase'event and relojBase = '1' then
+            if estadoActivo = '1' then
+                if cuentaMin = 9 and cuentaDecSec = 5 and cuentaUniSec = 9 then
+                    estadoActivo <= '0'; 	-- evalua si el estado activo es cero (activo)
+                else
+                    if cuentaUniSec = 9 then		-- mira si hemos llegado al limite de las unidades y 
+																-- suma una decena, y si hemos llegado al limite de las decenas entonces se agrega un
+																-- minuto, y si no se cumple ninguna entonces solo se añade una unidad
+                        cuentaUniSec <= (others => '0');
+                        if cuentaDecSec = 5 then
+                            cuentaDecSec <= (others => '0');
+                            cuentaMin <= cuentaMin + 1;
+                        else
+                            cuentaDecSec <= cuentaDecSec + 1;
+                        end if;
+                    else
+                        cuentaUniSec <= cuentaUniSec + 1;
+                    end if;
+                end if;
+            end if;
+            
+            cuenta_boton <= 0; -- al final la cuenta del botón se hace cero para evitar reconteos indeseados
+        end if;
     end process;
     
-    -- Traducción a vectores lógicos
     unidadesSec <= std_logic_vector(cuentaUniSec);
     decenasSec  <= std_logic_vector(cuentaDecSec);
     unidadesMin <= std_logic_vector(cuentaMin);
